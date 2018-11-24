ccf0ccae1dd69c1b8d9f6cfde49dadf012945949
Quentin de Longraye <quentin@dldl.fr>
2018-11-24 12:06:11 +0100

    fix content type header

--- src/index.js
+++ src/index.js
@@ -7,12 +7,16 @@ window.fetch = async (url, body) => {
 
   const response = await oldFetch(url, body)
   const originalResponse = response.clone()
-  const contentType = response.headers["Content-Type"]
+  const contentType = response.headers.get("Content-Type")
 
-  const fileName = `${md5(url + JSON.stringify(body))}.${contentType.split("/")[1]}`
+  const hash = md5(url + JSON.stringify(body))
+  const format = contentType.split("/")[1].split(";")[0]
+
+  const fileName = `${hash}.${format}`
+
   const downloadLink = document.createElement("a")
   downloadLink.download = fileName
-  downloadLink.href = `data:${response.headers["Content-Type"]};base64,${btoa(await response.text())}`
+  downloadLink.href = `data:${contentType};base64,${btoa(await response.text())}`
   downloadLink.click()
 
   return Promise.resolve(originalResponse)
